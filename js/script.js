43686af302a7e4c5673b491d7256d74b662b80e5
kent0071 <kent0071@i.ua>
2020-04-15 16:25:53 +0300

    add a section reviews

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+$(document).ready(function(){
+    $('.reviews__row').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        responsive: [
+            {
+              breakpoint: 775,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true
+              }
+            }
+          ]
+    });
+  });
+
